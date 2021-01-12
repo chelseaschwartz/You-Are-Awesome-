a6f2d0b0915bf0d3928b0dc7c808838d248dd487
Chelsea Schwartz <chelseaschwartz3@gmail.com>
2021-01-12 15:35:04 -0500

    Changed to GiddyUp!

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Chelsea Schwartz on 1/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
